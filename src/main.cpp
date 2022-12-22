113edfb088038b20a9721100e275a369a119ec52
SégolèneLazarus <seg.laz@ik.me>
2022-12-22 15:11:14 +0100

    structures dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
 #include "application_ui.h"
 #include "SDL2_gfxPrimitives.h"
 
+// j'ai tout mis dans le main pour l'instant parce que quand je mets dans un autre fichier j'ai plein d'erreurs et j'ai la flemme de tout régler mtn mais faudra mettre les struct autre part
+
+// structure de l'ellipse 
+struct Ellipse{
+    int coordX;
+    int coordY;
+    int vitX;
+    int vitY;
+    int rayon;
+    unsigned int compoRouge;
+    unsigned int compoVert;
+    unsigned int compoBleu;
+};
+
+// structure de la chaine d'ellipse
+struct ChaineEllipse{
+    Ellipse *ellipse;
+    int *nod; // je n'ai pas vu les noeuds donc j'ai aucune idée de comment ils fonctionnent
+};
+
+// structure du Mur
+struct Mur{
+    int coordX1;
+    int coordY1;
+    int coordX2;
+    int coordY2;
+};
+
+// pas finie je pense mais au moins y'a la base de la fonction (jsp si elle est utile mais je crois que si)
+void initialiseEllispe(Ellipse* ellipse){
+    ellipse->coordX = 0;
+    ellipse->coordY = 0;
+    ellipse->vitX = 0;
+    ellipse->vitY = 0;
+    ellipse->rayon = 0;
+    ellipse->compoRouge = 0;
+    ellipse->compoVert = 0;
+    ellipse->compoBleu = 0;
+}
 
 void draw(SDL_Renderer* renderer)
 {
@@ -53,6 +92,9 @@ int main(int argc, char** argv) {
         // GESTION ACTEURS
 
         // ...
+
+        // test affichage boule
+        int filledEllipseRGBA (SDL_Surface *dst, Sint16 x, Sint16 y, Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
         
         // EFFACAGE FRAME
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
